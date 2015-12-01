21b1437e3b0455970e9fe411f8630b0d5cd0ab30
Ben Johnson <ben@gophronesis.com>
2015-12-01 16:40:24 -0500

    cleaning up

--- wit/string-example.py
+++ wit/string-example.py
@@ -9,7 +9,7 @@ max_len      = 50
 # Generate fake dataset
 print 'WIT :: Generating data'
 f    = FakeData()
-data = f.dataframe(size = 5000)
+data = f.dataframe(size = 10000)
 
 # Format for keras training
 print 'WIT :: Formatting data'
@@ -19,11 +19,11 @@ train, levs = formatter.format(data, ['obj'], 'hash')
 # Compile and train classifier
 print 'WIT :: Compiling classifier'
 classifier = StringClassifier(train, levs)
-classifier.fit(batch_size = 300)
+classifier.fit(batch_size = 300, nb_epoch = 10)
 
 # Create test dataset
 print 'WIT :: Creating test dataset'
-testdata = f.dataframe(size = 5000)
+testdata = f.dataframe(size = 1000)
 test, _  = formatter.format(testdata, ['obj'], 'hash')
 
 # Make prediction on test dataset and check accuracy
@@ -39,10 +39,10 @@ print(pd.crosstab(pred_class, act_class))
 assert(classifier.classify_string('http://www.gophronesis.com') == 'url')
 assert(classifier.classify_string('ben@gophronesis.com')        == 'free_email')
 assert(classifier.classify_string('2000-01-01')                 == 'date')
-assert(classifier.classify_string('203-802-9283')               == 'phone_number')
-assert(classifier.classify_string('2038329382')                 == 'phone_number')
+assert(classifier.classify_string('1-203-802-9283')             == 'phone_number')
+assert(classifier.classify_string('12038329382')                == 'phone_number')
 assert(classifier.classify_string('241-34-1983')                == 'ssn')
-assert(classifier.classify_string('10.1.70.234')                == 'ipv4')
-assert(classifier.classify_string('andy46')                     == 'user_name')
+assert(classifier.classify_string('10.1.60.201')                == 'ipv4')
+assert(classifier.classify_string('randy46')                    == 'user_name')
 assert(classifier.classify_string('41177432883439283')          == 'credit_card_number')
 assert(classifier.classify_string('2001')                       == 'year')
